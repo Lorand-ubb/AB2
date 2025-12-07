e9b82a51335eb8fc252b62ef10d52b0f29d6ac01
Loránd-Tihamér Tőkés <lorand.tokes@stud.ubbcluj.ro>
2025-12-08 00:57:12 +0200

    Add files via upload

---
+++ lab3/Trigerek.sql
@@ -0,0 +1,87 @@
+﻿CREATE TRIGGER trg_UpdateUserBalanceIfTransactionMatch
+ON Transactions
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @UserSells NVARCHAR(50), @UserSellsID INT
+	DECLARE @UserBuys NVARCHAR(50), @UserBuysID INT
+
+	IF (SELECT TransactionType FROM inserted) = 'Buying'
+	BEGIN
+		EXECUTE UpdateUserBalanceOnBuying @pUserEmail = (SELECT UserEmail FROM inserted), @pTransactionValue = (SELECT TransactionValue FROM inserted)
+
+		SET @UserBuys = (SELECT UserEmail FROM inserted)
+		SET @UserBuysID = (SELECT TransactionID FROM inserted)
+		
+		SELECT t.* INTO #t1
+		FROM Transactions t, inserted i
+		WHERE t.ValidSkinID = i.ValidSkinID AND 
+			i.TransactionValue >= t.TransactionValue AND
+			t.TransactionType = 'Selling' AND 
+			t.TransactionStatus = 'Ongoing'
+			
+		SELECT TOP 1 * INTO #t2
+		FROM #t1
+		ORDER BY #t1.TransactionValue
+
+		SET @UserSells = (SELECT UserEmail FROM #t2)
+		
+	END --IF vege
+	ELSE 
+	BEGIN
+		SET @UserSells = (SELECT UserEmail FROM inserted)
+		SET @UserSellsID = (SELECT TransactionID FROM inserted)
+
+		SELECT t.* INTO #t1
+		FROM Transactions t, inserted i
+		WHERE t.ValidSkinID = i.ValidSkinID AND 
+			i.TransactionValue >= t.TransactionValue AND
+			t.TransactionType = 'Buying' AND 
+			t.TransactionStatus = 'Ongoing'
+			
+		SELECT TOP 1 * INTO #t2
+		FROM #t1
+		ORDER BY #t1.TransactionValue DESC
+
+		SET @UserBuys = (SELECT UserEmail FROM #t2)
+	END --Else vege
+	
+	--Tranzakciok statuszanak megvaltoztatasa
+	UPDATE Transactions
+		SET TransactionStatus = 'Succesfull'
+		WHERE TransactionID = @UserSellsID
+
+	UPDATE Transactions
+	SET TransactionStatus = 'Succesfull'
+	WHERE TransactionID = @UserBuysID
+
+	--ha a vasarlo fel a felajanlottnal kevesebbet kell fizessek akkor visszakapja a penzet
+	IF (SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserSellsID) != (SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserBuysID)
+	BEGIN
+		UPDATE Users
+		SET UserBalance = UserBalance + ((SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserBuysID) - (SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserSellsID))
+		WHERE UserEmail = @UserBuys
+	END
+
+	-- Ha a vasarlo kevesebbert vasarlot mint amenyit felajanlot akkor a tranzakcio ertekenek a modositasa
+	UPDATE Transactions
+	SET TransactionValue = (SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserSellsID)
+	WHERE TransactionID = @UserBuysID
+
+	--Tranzakciok utana UserBalance valtozasok elintezese
+	UPDATE Users
+	SET UserBalance = (SELECT TransactionValue FROM Transactions WHERE TransactionID = @UserSellsID)
+	WHERE UserEmail = @UserSells
+
+	--A skik kiosztasa a vasarlonak
+	INSERT INTO Own VALUES
+	(GETDATE(), @UserBuys, (SELECT ValidSkinID FROM Transactions WHERE TransactionID = @UserSellsID))
+
+	
+	DROP TABLE #t1
+	DROP TABLE #t2
+END
+
+
+--Tarolt eljarasok:
+--Visszaadja azt a userEmail-t aki a legnagyobb transactionValue-val rendelkezik
---
+++ lab3/tarolt_User.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE UpdateUserBalanceOnBuying
+	@pUserEmail NVARCHAR(50), 
+	@pTransactionValue INT
+AS
+BEGIN
+	SET NOCOUNT ON; 
+
+	--Parameter ellenorzes
+	IF NOT EXISTS (SELECT * FROM Users WHERE UserEmail = @pUserEmail)
+	BEGIN
+		PRINT 'Ez a User nem letezik'
+		RETURN
+	END
+
+	IF @pTransactionValue < 10
+	BEGIN
+		PRINT 'Ez az ertek tul kicsi'
+		RETURN
+	END
+
+		
+	IF (SELECT UserBalance FROM Users WHERE UserEmail = @pUserEmail) >= @pTransactionValue 
+	BEGIN 
+		UPDATE Users 
+		SET UserBalance = UserBalance - @pTransactionValue 
+		FROM Users u 
+		WHERE u.UserEmail = @pUserEmail 
+	END
+	ELSE
+	BEGIN
+		PRINT 'The User does not have enough money for this transaction'
+	END
+END
+GO
